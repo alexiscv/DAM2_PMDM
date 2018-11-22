fd0245d427bf61908001403ce15c18f4579ec001
alexiscv <hogo.jp@gmail.com>
2018-11-22 15:07:00 +0100

    Ejercicio terminado*

--- T4Ej16_EditText_ImageView_ImageButton/app/src/main/java/com/example/angel/t4ej16_edittext_imageview_imagebutton/MainActivity.java
+++ T4Ej16_EditText_ImageView_ImageButton/app/src/main/java/com/example/angel/t4ej16_edittext_imageview_imagebutton/MainActivity.java
@@ -4,31 +4,44 @@ import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // Atributos
     private int cuenta = 0;
+    String[] escalas;
+    ImageView imagenPrincipal;
+    TableRow rowMiniaturas = null;
+    Drawable[] imagenes = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Referencias
+        imagenPrincipal = findViewById(R.id.fotoPrincipal);
+        AutoCompleteTextView tvEscalados = findViewById(R.id.escalas);
+        rowMiniaturas = findViewById(R.id.miniaturas);
+
         /**
          * Creamos el adapter para cargar las sugerencias en el AutocompleteTextView
          */
-        // Referenciamos
-        AutoCompleteTextView tvEscalados = findViewById(R.id.escalas);
-
         // Cargamos el Array de Strings
-        String[] escalas = getResources().getStringArray(R.array.escalas);
+        escalas = getResources().getStringArray(R.array.escalas);
 
         // Creamos el Adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, escalas);
@@ -39,35 +52,36 @@ public class MainActivity extends AppCompatActivity {
         /**
          * Mostramos las imagenes del Array de Imagenes
          */
-        // Referenciamos el elemento padre
-        TableRow rowMiniaturas = findViewById(R.id.miniaturas);
-
         // Obtengo el ancho máximo de la pantalla del dispositivo para luego situar los botones
         int anchoPantalla = getWindowManager().getDefaultDisplay().getWidth();
 
         // Array de imagenes
-        Drawable[] imagenes = cargarImagenes();
+        imagenes = cargarImagenes();
 
-        // Creamos el Params para las ImageView calculando el espacio que ocupará cada imagen
+        // Creamos el Params para las ImageView (miniaturas) calculando el espacio que ocupará cada imagen
         // Lo calculamos dividiendo el tamaño de la pantalla entre el número de elementos
-        TableRow.LayoutParams params = new TableRow.LayoutParams((anchoPantalla / imagenes.length), 120);
+        TableRow.LayoutParams params = new TableRow.LayoutParams((anchoPantalla/imagenes.length), 100);
+
+        // Cargamos la primera imagen del array de imagenes como imagen principal por defecto
+        imagenPrincipal.setImageDrawable(imagenes[0]);
 
         // Recorremos las imagenes...
         for (Drawable img : imagenes) {
 
             // ... Por cada imagen, creamos un ImageView y cargamos el recurso
-            ImageButton imagen = new ImageButton(this);
-            imagen.setLayoutParams(params);
-            imagen.setId(cuenta);
-            imagen.setImageDrawable(img);
-            imagen.setAdjustViewBounds(true); // Para que la imagen no se deforme
-            imagen.setScaleType(ImageView.ScaleType.CENTER_INSIDE); // Tipo de escalado
+            ImageButton imagen = new ImageButton(this);         // Creamos la imagen
+            imagen.setId(cuenta);                                       // ID
 
-            // Añadimos la imagen al contenedor
-            rowMiniaturas.addView(imagen);
+            params.leftMargin = 3;                                      // Margen izquierdo
+            params.rightMargin = 3;                                     // Margen derecho
 
-            // Generamos el siguiente ID de la IMG
-            cuenta++;
+            imagen.setLayoutParams(params);                             // Asignamos el params
+            imagen.setImageDrawable(img);                               // Indicamos el recurso gráfico
+
+            imagen.setAdjustViewBounds(true);                           // Para que la imagen no se deforme
+            imagen.setScaleType(ImageView.ScaleType.CENTER_INSIDE);     // Tipo de escalado
+
+            imagen.setBackgroundResource(R.color.verdeOscuro);          // Fondo de color
 
             // Creamos un Listener para esta imagen
             imagen.setOnClickListener(new View.OnClickListener() {
@@ -75,13 +89,60 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
 
                     // Sustituimos la imagen principal, por la imagen que hemos pulsado
-                    ImageView imagenPrincipal = findViewById(R.id.fotoPrincipal);
                     imagenPrincipal.setImageDrawable(((ImageView) v).getDrawable());
 
                 }
             });
 
+            // Generamos el siguiente ID de la IMG
+            cuenta++;
+
+            // Añadimos la imagen al contenedor
+            rowMiniaturas.addView(imagen);
+
         }
+
+        /**
+         * Imagen principal
+         */
+        // Como cada row reparte el espacio automáticamente
+        // Tenemos que asignarle a la imagen principal el número total de columnas que debe ocupar
+        // Que será, el numero de miniaturas del array de imagenes
+        // Lo primero es cargar sus parametros en un Params, para poder modificarlos
+        TableRow.LayoutParams paramsImgPrincipal = (TableRow.LayoutParams) imagenPrincipal.getLayoutParams();
+
+        // Ahora indicamos el número de columnas que debe de ocupar el ImageView (img principal)
+        paramsImgPrincipal.span = imagenes.length;
+
+        // Y lo aplicamos
+        imagenPrincipal.setLayoutParams(paramsImgPrincipal);
+
+        /**
+         * Creamos un escuchador WATCHER para el EditText
+         */
+        tvEscalados.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                // Cuando el texto cambie, comprobamos que el valor del
+                // AutoCompleteTextView sea válido
+                if (recorteValido(s)) {
+                    // Aplicamos el recorte
+                    imagenPrincipal.setScaleType(ImageView.ScaleType.valueOf(s.toString()));
+                }
+
+            }
+        });
 
     }
 
@@ -102,6 +163,27 @@ public class MainActivity extends AppCompatActivity {
 
         // Retornamos el array de Drawrables
         return arrayImagenes;
+
+    }
+
+    private boolean recorteValido(Editable recorte) {
+
+        // Buscamos el recorte escrito con las opciones disponibles
+        for (String tipoEscalado : escalas) {
+
+            // Miramos si la opción es válida...
+            if (recorte.toString().equalsIgnoreCase(tipoEscalado)) {
+                return true;
+            }
+
+        }
+
+        // Notificamos que no hay coincidencias
+        Toast mensaje = Toast.makeText(this, "No existe este recorte", Toast.LENGTH_SHORT);
+        mensaje.show();
+
+        // Si no hemos encontrado coincidencias...
+        return false;
 
     }
 }
